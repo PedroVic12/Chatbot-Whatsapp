3a895debdcc6f78e39aa18ed6cc341d40131f6d5
PedroVic12 <pedrovictor.rveras12@gmail.com>
2023-01-13 19:50:37 -0300

    .

--- Chatbot - Delivary e Entregas/Chatbot Kyogre - Refatoração com IA e JavaScript Juncional/src/Robo/Kyogre.js
+++ Chatbot - Delivary e Entregas/Chatbot Kyogre - Refatoração com IA e JavaScript Juncional/src/Robo/Kyogre.js
@@ -6,11 +6,14 @@ const qrcode = require('qrcode-terminal');
 
 // Instanciando o Objeto com o nome do Cliente
 const whatsapp = new Client({
-    authStrategy: new LocalAuth({ clientId: "Citta-Lanchonete" })
+    authStrategy: new LocalAuth({ clientId: "BigBi-Citta" })
 });
 //const whatsapp = new Client();
 
 //! Funções anonimas
+let numero_estagio = 1
+
+
 function enviarMensagem(whatsapp, message, text) {
     return whatsapp.sendMessage(message.from, text);
 }
@@ -25,30 +28,35 @@ function enviarBotao(whatsapp, message, text, buttons) {
     return whatsapp.sendMessage(message.from, botoes);
 }
 
-const numero_estagio = 1
 function avancarEstagio() {
     numero_estagio++
-    console.log("numero do estagio = ", numero_estagio)
+    console.log("Avançando o estágio!")
 }
 
+function getHoras() {
+    let data_atual = new Date();
+    let hora = data_atual.getHours();
+    let minuto = data_atual.getMinutes();
+
+    return hora + ":" + minuto
+}
 
 
 //!ESTAGIOS
 const Estagio1 = new (class {
     constructor() {
         // código da classe aqui
-        this.pedido_iniciado = false
-        this.numero_pedido = 0
-
-        this.nextLevel = false
     };
     conectandoWpp = () => {
         return new Promise((resolve, reject) => {
             console.log("\nIniciando o Chatbot...")
             console.log('Gerando QR code...');
+
             whatsapp.on('qr', qr => {
                 qrcode.generate(qr, { small: true });
             });
+
+
             whatsapp.on('ready', () => {
                 console.log('whatsapp pronto! Pode usar agora :)');
                 resolve(whatsapp);
@@ -56,7 +64,6 @@ const Estagio1 = new (class {
             whatsapp.initialize();
         });
     };
-
 
     recebeMensagem() {
 
@@ -76,242 +83,160 @@ const Estagio1 = new (class {
             console.log("Nome do Cliente = ", nome)
             console.log("Número do Usuário = " + telefone);
             console.log("Mensagem recebida = " + message.body); //Salvar dentro de uma lista para usar I.A depois
-            //console.log("Fluxo Atual =  ", this.estagio)
-            //console.log(` ${this.pedido_iniciado}`)
+            console.log("Fluxo Atual =  ", numero_estagio)
             console.log("\n")
         });
 
     };
 
-    getHoras() {
-        let data_atual = new Date();
-        let hora = data_atual.getHours();
-        let minuto = data_atual.getMinutes();
-
-        return hora + ":" + minuto
-    }
 
 
-    infoCliente(message) {
-        horario_pedido = this.getHoras()
-        //let nome_cliente = this.getNome(whatsapp, message);
-        let telefone_cliente = message.from.split('@')[0]
-        this.numero_pedido++
-
-        return {
-            "horario_pedido": horario_pedido,
-            "telefone_cliente": telefone_cliente
-        }
-    }
-
-    getNome(whatsapp, message) {
-
-        enviarMensagem(whatsapp, message, "Seja bem vindo! Antes de começarmos, por favor, digite seu *Nome*:")
-        let nome_cliente;
-        nome_cliente = message.body;
-        this.pedido_iniciado = true
-        return nome_cliente;
-    }
-
-
-    async getNomeCLiente(whatsapp, message) {
-        let nome_cliente;
-
-        while (this.pedido_iniciado === false) {
-            console.log("Aguardando resposta do usuário...");
-
-            nome_cliente = await new Promise(resolve => {
-                whatsapp.on("message", response => {
-                    if (response.from === message.from) {
-                        this.pedido_iniciado = true;
-
-                        nome_cliente = message.body;
-
-                        resolve(response.body);
-                    }
-                });
-            });
-        }
-
-        return nome_cliente
-    }
-
-    async setCliente(whatsapp, message) {
-
-        //Fazendo um Objeto Promise
-        enviarMensagem(whatsapp, message, `SET CLIENTE `
-        ).then((response) => {
-            return response
-
-            //Retornando a resposta do Objeto 
-        }).then(() => {
-            console.log('Cliente cadastrado com sucesso')
-        })
-
-
-        return "Cliente cadastrado com sucesso NA MENSAGEM"
-    }
-
-
-
-    // ===================================
-    funcaoPrincipal() {
-        // código aqui
-        const nome = getCliente_ChatBot();
-
-        // código que depende do nome do cliente
-        return nome
-    }
-
-    async getCliente_ChatBot(whatsapp, message) {
-        // envia a mensagem para o usuário pedindo o nome
-        await enviarMensagem(whatsapp, message, "Por favor, digite seu nome:");
-        // espera a resposta do usuário
-        const response = await esperarResposta(whatsapp, message);
-        return response.body;
-    }
-
-    async esperarResposta(whatsapp, message) {
-        let nome_cliente;
-
-        await enviarMensagem(whatsapp, message, "Seja bem vindo! Antes de começarmos, por favor, digite seu *Nome*:")
-
-        while (this.pedido_iniciado === false) {
-            console.log("Aguardando resposta do usuário...");
-            nome_cliente = await new Promise(resolve => {
-                whatsapp.on("message", response => {
-                    if (response.from === message.from) {
-                        this.pedido_iniciado = true;
-                        resolve(response.body);
-                    }
-                });
-            });
-        }
-
-        return nome_cliente
-    }
-
-    // getCliente(whatsapp, message) {
-    //     let nome_whatsapp = message._data.notifyName;
-    //     const nome = this.getNome(whatsapp, message)
-
-    //     if (nome.toUpperCase() in nome_whatsapp.toUpperCase()) {
-    //         this.pedido_iniciado = true
-    //         enviarMensagem(whatsapp, message, `OLA MUNDO ${nome}`)
-
-    //     }
-
-    //     //procurar o cliente na base de dados
-    // }
     boasVindas(whatsapp, message) {
-        // Boas Vindas
-        enviarMensagem(whatsapp, message, `Bem-vindo a Citta Lanchonete! Obrigado por escolher a nossa lanchonete. \n Eu sou o Robô Groundon e estou aqui para ajudá-lo. `
-        );
-        enviarMensagem(whatsapp, message, "Antes de começarmos, por favor, digite seu *nome*:");
+        enviarMensagem(whatsapp, message, 'Olá, seja bem vindo ao Groundon!');
+        enviarMensagem(whatsapp, message, 'Meu nome é Groundon, sou um chatbot e estou aqui para te ajudar a fazer seu pedido!');
+        enviarMensagem(whatsapp, message, "Antes de começarmos, por favor, digite seu nome:");
 
-        //Pegando o nome do cliente
-        let nome_cliente = this.getNome(whatsapp, message);
-        enviarMensagem(whatsapp, message, `Um prazer te conhecer, ${nome_cliente}`)
     }
 
-    boasVindas24 = async (whatsapp, message) => {
-        try {
-            const nome = await this.getNome(whatsapp, message)
-            enviarMensagem(whatsapp, message, "Um prazer te conhecer, ${nome}").then(() => {
-                console.log("Mensagem enviada")
-            })
-        } catch (err) {
-            console.log(err)
-        }
-    };
 
-
-
-
-    // Função para mostrar o cardápio ao usuário
-
-
-
-    mostrarCardapio(whatsapp, message) {
-        // Verifica se a mensagem do usuário é "ver cardápio"
-        if (message.body.toLowerCase() === 'ver cardápio') {
-            // Define o cardápio
-            const cardapio = [
-                {
-                    nome: 'Refrigerante',
-                    preco: 3.50,
-                },
-                {
-                    nome: 'Suco',
-                    preco: 4.00,
-                },
-                {
-                    nome: 'Água Mineral',
-                    preco: 2.50,
-                },
-            ];
-            // Transforma o cardápio em uma lista de strings
-            const opcoes = cardapio.map(bebida => `${bebida.nome} - R$${bebida.preco}`).join('\n');
-            // Envia a mensagem com o cardápio para o usuário
-            enviarMensagem(whatsapp, message, `Aqui está o nosso cardápio: \n\n${opcoes}\n\nPor favor, digite o nome da bebida que deseja pedir:`);
-        }
-    }
 
 });
 
 
-const estagio2 = new (class {
+const Estagio2 = new (class {
     constructor() {
-        this.pedido_iniciado = false;
+        this.numero_pedido = 1
+        this.nome_cliente = ""
+    }
+
+
+    async getNomeCliente(whatsapp, message) {
+        try {
+            this.nome_cliente = message.body
+            enviarMensagem(whatsapp, message, `Prazer em te conhecer, ${this.nome_cliente}!`);
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    getNome() {
+        return this.nome_cliente;
+    }
+
+    infoCliente(whatsapp, message) {
+        let horario_pedido = Estagio1.getHoras()
+        let nome = this.getNomeCliente(whatsapp, message);
+        let telefone_cliente = message.from.split('@')[0]
+
+        this.numero_pedido++
+
+        return {
+            "Nome": nome,
+            "Numero de Pedidos": this.numero_pedido,
+            "horario do pedido": horario_pedido,
+            "Telefone": telefone_cliente
+        }
+    }
+
+
+
+
+})
+
+
+const Estagio3 = new (class {
+    constructor() {
+        this.pedido_iniciado === false
+    }
+
+    //adicionar ao carrinho
+
+    //confirmar pedido;
+
+    setClienteBase() {
+        if (this.pedido_iniciado === true) {
+
+            console.log("Pedido Iniciado!")
+            console.log(`Cliente ${Estagio2.getNome()} sendo cadastrado na base de dados!`)
+        }
+    }
+
+    mostrarCardapioNoChat(whatsapp, message) {
+        // Define o cardápio
+        const cardapio = [
+            {
+                nome: 'Refrigerante',
+                preco: 3.50,
+            },
+            {
+                nome: 'Suco',
+                preco: 4.00,
+            },
+            {
+                nome: 'Água Mineral',
+                preco: 2.50,
+            },
+        ];
+        // Transforma o cardápio em uma lista de strings
+        const opcoes = cardapio.map(bebida => `${bebida.nome} - R$${bebida.preco}`).join('\n');
+        // Envia a mensagem com o cardápio para o usuário
+        enviarMensagem(whatsapp, message, `Aqui está o nosso cardápio: \n\n${opcoes}\n\nPor favor, digite o nome da bebida que deseja pedir:`);
+
     }
 
     mostrarMenuPrincipal = (whatsapp, message) => {
-        enviarBotao(whatsapp, message, "Escolha uma opção", [
-            { body: "Ver Cardápio" },
-            { body: "Fazer Pedido" },
-            { body: "Ver nossa Localização" }
-        ]);
+        enviarMensagem(whatsapp, message, "Escolha uma opção abaixo do que voce deseja");
+
+        // enviarBotao(whatsapp, message, "Vamos lá, ${Estagio2.getNomeCliente()}! Escolha uma opção abaixo do que voce deseja", [
+        //     { body: "Ver Cardápio" },
+        //     { body: "Fazer Pedido" },
+        //     { body: "Ver nossa Localização" }
+        // ], "Chatbot Groundon', `Horário de Atendimento = ${Estagio2.getHoras()}");
+
+        enviarBotao(whatsapp, message, "Vamos lá, ${Estagio2.getNomeCliente()}! Escolha uma opção abaixo do que voce deseja",
+            [
+                { body: "Ver Cardápio" },
+                { body: "Fazer Pedido" },
+                { body: "Ver nossa Localização" }
+            ]
+        );
     }
 
     mandarMensagemTeste(whatsapp, message) {
 
-        enviarMensagem(whatsapp, message, "Teste de mensagem")
-
-        let button = new Buttons(`Vamos lá, ${nome_cliente}! Escolha uma opção abaixo do que voce deseja`, [
+        let button = new Buttons(`Vamos lá, ${Estagio2.getNome()}! Escolha uma opção abaixo do que voce deseja`, [
             { body: 'Ver Cardápio' },
             { body: 'Fazer Pedido' },
             { body: 'Ver nossa Localização' }
-        ], 'Chatbot Groundon', `Horário de Atendimento = ${this.getHoras()}`);
+        ], 'Chatbot Groundon', `Horário de Atendimento = ${getHoras()}`);
 
         return whatsapp.sendMessage(message.from, button)
 
     }
 
 
-    //fazer o pedido
+    mostrarLocal(whatsapp, message) {
+        let botafogo = {
+            nome: 'Botafogo',
+            rua1: 'Rua Praia de botafogo, 340',
+            rua2: 'Rua Voluntários da Pátria, 156',
+            rua3: 'Rua Voluntários da Pátria, 350'
+        }
 
-    //adicionar ao carrinho
+        enviarMensagem(whatsapp, message, `Aqui estão as nossas localizações: \n ${botafogo.nome} \n ${botafogo.rua1} \n ${botafogo.rua2} \n ${botafogo.rua3}`)
 
-    //confirmar pedido;
+    }
 })
 
-//! Estágio 3 - Pedido
 
-const estagio3 = new (class {
+//! Estagio 4 -  Processa o pedido e Pagamento
+const Estagio4 = new (class {
     constructor() {
         // código da classe aqui
     }
 
-
-    mostrarMenu = (whatsapp, message) => {
-        enviarBotao(whatsapp, message, "Escolha uma opção", [
-            { body: "Ver Cardápio" },
-            { body: "Fazer Pedido" },
-            { body: "Ver nossa Localização" }
-        ]);
-    }
-
-    fazerPedido2(whatsapp, message) {
+    fazerPedido(whatsapp, message) {
         let button = new Buttons("Tudo bem, ${nome_cliente}? Escolha uma opção abaixo do que voce deseja", [
             { body: 'Sanduiche' },
             { body: 'Bebidas' },
@@ -394,106 +319,97 @@ const estagio3 = new (class {
 
     }
 
-    mostrarLocal(whatsapp, message) {
-        botafogo = {
-            nome: 'Botafogo',
-            rua1: 'Rua Praia de botafogo, 340',
-            rua2: 'Rua Voluntários da Pátria, 156',
-            rua3: 'Rua Voluntários da Pátria, 350'
-        }
-
-        enviarMensagem(whatsapp, message, `Aqui estão as nossas localizações: \n ${botafogo.nome} \n ${botafogo.rua1} \n ${botafogo.rua2} \n ${botafogo.rua3}`)
+    adicionarItemCarrinho() {
 
     }
-
-}
-
-    //chatbot.ProcessaPedido() -> joga na base de dados
+    //chatbot.ProcessaPagamento() -> joga na base de dados
 
     //enviarPedido (pegar localização)
 
     //chatbot.notaFiscal() 
 
-)
-
-
-//! Estagio 4 - Pagamento
-
+})
 
 //! Estagio 5 - Entrega e Resumo
 
 
 //!================ FUNÇÃO PRINCIPAL =====================
-Estagio1.conectandoWpp().then(() => {
-    // Código a ser executado após a promise ser resolvida
-    console.log('Conectado com sucesso!')
+Estagio1.conectandoWpp()
+    .then(() => {
+        // Código a ser executado após a promise ser resolvida
+        console.log('Conectado com sucesso!')
 
-});
-
-
-//Evento Listener esperando mensagem (loop)
+    })
+    .catch((error) => {
+        // Código a ser executado após a promise ser rejeitada
+        console.log("Ops Deu Problema ao conectar!")
+        console.log(error)
+    })
 Estagio1.recebeMensagem()
-whatsapp.on('message', message => {
 
-    //! ===================== Estágio 1 - Apresentação =====================
+async function iniciarBot() {
 
-    // promise manda msg
+    //Evento Listener esperando mensagem (loop)
+    whatsapp.on('message', async message => {
 
-    // then tenta pegar o nome do cliente
+        //! ===================== Estágio 1 - Apresentação =====================
+        if (numero_estagio === 1) {
 
-    // finalmente mostra o cardapio e pedido iniciado
+            Estagio1.boasVindas(whatsapp, message)
 
+            avancarEstagio()
+        }
 
-    console.log(numero_estagio)
+        //!=====================  Estágio 2 - Mostrar Opções =====================
+        else if (numero_estagio === 2) {
 
-    while (numero_estagio < 5) {
+            Estagio2.getNomeCliente(whatsapp, message)
 
-        switch (numero_estagio) {
-            case 1:
-                console.log("Estagio 1")
-                Estagio1.boasVindas24(whatsapp, message)
+            // let info = Estagio2.infoCliente(whatsapp, message)
+            // console.log(info)
 
-                enviarMensagem(whatsapp, message, `Bem-vindo a Citta Lanchonete! Obrigado por escolher a nossa lanchonete. \n\n Eu sou o Robô Groundon e estou aqui para ajudá-lo. `);
-                enviarMensagem(whatsapp, message, `Horário de Atendimento = ${Estagio1.getHoras()}`)
-                enviarMensagem(whatsapp, message, "Seja bem vindo! Antes de começarmos, por favor, digite seu *Nome*:")
+            //Estagio2.setCliente(whatsapp, message)
 
-                whatsapp.sendMessage(message.from, "DEBUG JS")
+            enviarMensagem(whatsapp, message, "Vamos iniciar o atendimento!")
+            avancarEstagio()
 
+        }
+
+        //!=====================  Estágio 3 - Mostra o Menu e Faz o pedido  =====================
+        else if (numero_estagio === 3) {
+
+            Estagio3.pedido_iniciado = true
+
+            //Estagio3.mostrarMenuPrincipal(whatsapp, message)
+            Estagio3.mandarMensagemTeste(whatsapp, message)
+
+            if (message.body === 'Ver Cardápio') {
+                Estagio3.mostrarCardapioNoChat(whatsapp, message);
+            }
+
+            if (message.body === 'Fazer Pedido') {
                 avancarEstagio()
-                break;
 
-            case 2:
-                console.log("Estagio 2")
-            default:
-                break;
+            }
+
+            if (message.body === 'Ver nossa Localização') {
+                Estagio3.mostrarLocal(whatsapp, message)
+            }
+        }
+
+        //!=====================  Estágio 4 - Processa o pedido e Pagamento =====================
+        else if (numero_estagio === 4) {
+            Estagio4.fazerPedido(whatsapp, message)
         }
 
 
-    }
+    });
+}
+iniciarBot()
 
 
 
 
-    //!=====================  Estágio 2 - Mostrar Opções =====================
-
-    estagio2.mostrarMenuPrincipal(whatsapp, message)
-
-    //!=====================  Estágio 3 - Faz o pedido e processa o pedido =====================
-    estagio3.mostrarMenu(whatsapp, message);
-
-    if (message.body === 'ver cardápio') {
-        estagio3.mostrarMenu(whatsapp, message);
-    }
-
-    if (message.body === 'fazer pedido') {
-        estagio3.fazerPedido2(whatsapp, message)
-
-    }
-
-    if (message.body === 'Ver Nossa Localização') {
-        estagio3.mostrarLocal(whatsapp, message)
-    }
-});
 
 
-module.exports = { Estagio1, estagio2, estagio3 };
+module.exports = { Estagio1, Estagio2, Estagio3 };
