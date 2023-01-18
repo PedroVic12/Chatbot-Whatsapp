aec97ce3958bbae51944855979a362a8c1f59fff
PedroVic12 <pedrovictor.rveras12@gmail.com>
2023-01-18 10:59:32 -0300

    .

--- Chatbot - Delivary e Entregas/Chatbot Groundon Dev/src/Front End/main.dart.js
+++
@@ -1,9 +0,0 @@
-
-<script src="main.dart.js" type="application/javascript">
-    let useHtml = // ...
-    if(useHtml) {
-        window.flutterWebRenderer = "html";
-} else {
-        window.flutterWebRenderer = "canvaskit";
-}
-</script>
