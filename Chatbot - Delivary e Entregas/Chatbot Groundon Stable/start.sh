22bb71e7ed1828f9e91ce93c69928c6bc66679bd
Pedro Victor <pedrovictorveras@id.uff.br>
2023-02-10 13:38:53 -0300

    Update start.sh

--- Chatbot - Delivary e Entregas/Chatbot Groundon Stable/start.sh
+++ Chatbot - Delivary e Entregas/Chatbot Groundon Stable/start.sh
@@ -1,7 +1,7 @@
 
 
 
-cd /home/pedrov/Documentos/GitHub/Chatbot-Whatsapp/Chatbot - Delivary e Entregas/Chatbot Groundon Dev/src/Robo
+cd /home/pedrov/Documentos/GitHub/Chatbot-Whatsapp/Chatbot - Delivary e Entregas/Chatbot Groundon Stable/src/Robo
 
 echo "Executar o Docker";
 systemctl start docker
