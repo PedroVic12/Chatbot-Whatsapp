364dd6a1717682226a3b13c046f26d5eaa5084b1
PedroVic12 <pedrovictor.rveras12@gmail.com>
2023-06-09 14:53:55 -0300

    .

--- Chatbot - Delivary e Entregas/Chatbot Rayquaza x Groundon x Kyogre/Rayquaza/src/Server Python/RayquazaWebSocket.py
+++ Chatbot - Delivary e Entregas/Chatbot Rayquaza x Groundon x Kyogre/Rayquaza/src/Server Python/RayquazaWebSocket.py
@@ -3,7 +3,6 @@ from fastapi.responses import HTMLResponse
 import uvicorn
 import json
 import os
-from datetime import date
 
 app = FastAPI()
 
@@ -11,10 +10,6 @@ app = FastAPI()
 class Rayquaza:
     def __init__(self):
         self.connections = []
-
-    async def enviarPedido(self):
-        # Implemente aqui a lógica para enviar o pedido para o aplicativo Flutter
-        pass
 
     async def receberPedido(self, pedido):
         # Implemente aqui a lógica para processar as informações do pedido recebido
@@ -25,49 +20,32 @@ class Rayquaza:
         for connection in self.connections:
             await connection.send_text("Pedido recebido: " + pedido)
 
-    async def enviarStatusPedido(self, status):
-        # Implemente aqui a lógica para enviar atualizações de status do pedido para o cliente
-        # Como exemplo, apenas exibimos a mensagem de atualização de status
-        print("Atualização de status:", status)
-
-        # Exemplo de envio de mensagem para todos os clientes conectados
-        for connection in self.connections:
-            await connection.send_text("Status do pedido: " + status)
-
-    async def receberInformacoes(self, informacoes):
-        # Implemente aqui a lógica para processar as informações recebidas do chatbot
-
-        # Executa alguma ação com base nas informações recebidas
-        # ...
-
-        # Exemplo de envio de mensagem para todos os clientes conectados
-        for connection in self.connections:
-            await connection.send_text("Informações recebidas: " + informacoes)
-
-    async def verificarStatusChatbot(connections):
-        return len(connections) >= 1
+    async def enviarPedido(self, pedido):
+        # Implemente aqui a lógica para enviar o pedido para o aplicativo Flutter
+        pass
 
     async def websocket_endpoint(self, websocket: WebSocket):
         await websocket.accept()
         self.connections.append(websocket)
 
         while True:
-            # Aguarda a recepção de uma mensagem do cliente
-            message = await websocket.receive_text()
-
-            # Processa a mensagem (por exemplo, salvar em um arquivo JSON)
-            json_filename = "repository/pedido_09-06-23.json".format(
-                date.today().strftime("%d-%m-%y"))
-            with open(json_filename, "w") as file:
-                file.write(message)
-
-            # Verifica se o arquivo JSON tem a data de hoje
-            if os.path.exists(json_filename):
+            # Verifica se há um arquivo JSON presente no diretório "repository"
+            json_files = [f for f in os.listdir(
+                "repository") if f.endswith(".json")]
+            if json_files:
+                json_filename = os.path.join("repository", json_files[0])
                 with open(json_filename, "r") as file:
                     json_data = json.load(file)
 
                 # Envia os dados do JSON para o cliente
                 await websocket.send_json(json_data)
+
+                # Remove o arquivo JSON após lê-lo
+                os.remove(json_filename)
+
+            # Aguarda a recepção de uma mensagem do cliente
+            message = await websocket.receive_text()
+            await self.receberPedido(message)
 
     def run(self):
         # Rota para a página inicial
@@ -75,28 +53,7 @@ class Rayquaza:
         async def get():
             return HTMLResponse("<h1>Rayquaza Server</h1>")
 
-        @app.post("/enviarJson")
-        async def receber_json(json_data: dict):
-            # Processa o arquivo JSON recebido
-            # Por exemplo, você pode salvar o arquivo em disco
-            with open("repository/pedido_29-05-23.json", "w") as file:
-                json.dump(json_data, file)
-
-                # Exibe o conteúdo do JSON no servidor
-                print("JSON recebido:")
-                print(json_data)
-
-            # Envia uma resposta de confirmação ao cliente
-            return {"message": "Arquivo JSON recebido com sucesso!"}
-
-        @app.websocket("/status-chatbot")
-        async def websocket_status_chatbot(websocket: WebSocket):
-            await websocket.accept()
-            status = self.verificarStatusChatbot(rayquaza.connections)
-            await websocket.send_text("Status do chatbot: " + str(status))
-
         # Rota para estabelecer a conexão WebSocket
-
         @app.websocket("/ws")
         async def websocket_handler(websocket: WebSocket):
             await self.websocket_endpoint(websocket)
