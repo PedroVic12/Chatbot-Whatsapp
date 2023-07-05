631256e0ed73398e1b7cfbcc6f4f8d6d279b8078
PedroVic12 <86419935+PedroVic12@users.noreply.github.com>
2023-07-05 16:49:50 -0300

    TENTAR FUNCIONAR

--- Chatbot - Delivary e Entregas/Chatbot Rayquaza x Groundon x Kyogre/Rayquaza/src/Teoria do Caos/CHATBOT - IA - TERMINAL/Chatbot-Groundon-10.js
+++ Chatbot - Delivary e Entregas/Chatbot Rayquaza x Groundon x Kyogre/Rayquaza/src/Teoria do Caos/CHATBOT - IA - TERMINAL/Chatbot-Groundon-10.js
@@ -1,9 +1,16 @@
 const readline = require('readline-sync');
+const Widgets = require('./widgets/Widgets');
+const MenuCategorias = require('./widgets/MenuCategorias');
+const MenuCardapio = require('./widgets/MenuCardapio');
 
 class Usuario {
   constructor() {
     this.numero_estagio = 1;
     this.cliente = {};
+    this.menuCategorias = new MenuCategorias();
+    this.menuCardapio = new MenuCardapio();
+    this.widgets = new Widgets();
+    this.carrinho = [];
     // Adicione outras propriedades necessárias para o funcionamento do chatbot
   }
 
@@ -18,51 +25,78 @@ class Usuario {
         this.numero_estagio = 2;
       } else if (this.numero_estagio === 2) {
         console.log('\nEstágio 2: Mostrando o Menu Principal');
-        console.log('1. Ver cardápio em PDF');
-        console.log('2. Fazer um pedido');
-        console.log('3. Ver localização');
+        const menuPrincipal = this.widgets.menuPrincipal;
+        const menuPrincipalText = this.widgets.getMenuText('Menu Principal', menuPrincipal);
+        console.log(menuPrincipalText);
+
         let escolha = readline.question('Digite o número da opção desejada: ');
-        if (escolha === '1') {
-          console.log('Mostrando o cardápio em PDF...');
-        } else if (escolha === '2') {
-          console.log('Fazendo um pedido...');
+
+        if (escolha === 'cancelar') {
+          console.log('Pedido cancelado.');
+          break;
+        } else if (parseInt(escolha) >= 1 && parseInt(escolha) <= menuPrincipal.length) {
+          const opcaoEscolhida = menuPrincipal[parseInt(escolha) - 1];
+          console.log(`Opção escolhida: ${opcaoEscolhida.button.text}`);
+          // Adicione a lógica para o próximo estágio ou ação
+          this.numero_estagio = 3;
+        } else {
+          console.log('Opção inválida. Por favor, tente novamente.');
+        }
+      } else if (this.numero_estagio === 3) {
+        console.log('\nEstágio 3: Escolhendo a categoria');
+        const menuCategorias = this.menuCategorias.mostrarCategorias();
+        console.log(menuCategorias);
+
+        let escolha = readline.question('Digite o número da categoria desejada: ');
+
+        if (escolha === 'cancelar') {
+          console.log('Pedido cancelado.');
+          break;
+        } else if (parseInt(escolha) >= 1 && parseInt(escolha) <= this.menuCategorias.categorias.length) {
+          const categoriaEscolhida = this.menuCategorias.obterCategoria(parseInt(escolha));
+          console.log(`Categoria escolhida: ${categoriaEscolhida.nome}`);
+          // Adicione a lógica para o próximo estágio ou ação
           this.numero_estagio = 4;
-        } else if (escolha === '3') {
-          console.log('Mostrando a localização...');
         } else {
           console.log('Opção inválida. Por favor, tente novamente.');
         }
       } else if (this.numero_estagio === 4) {
-        console.log('\nEstágio 4: Escolhendo o tipo de produto');
-        console.log('1. Comida');
-        console.log('2. Bebida');
-        let escolha = readline.question('Digite o número da opção desejada: ');
-        if (escolha === '1') {
-          console.log('Mostrando o cardápio de comidas...');
-        } else if (escolha === '2') {
-          console.log('Mostrando o cardápio de bebidas...');
+        console.log('\nEstágio 4: Escolhendo o produto');
+        const menuProdutos = this.menuCardapio.exibirMenu();
+        console.log(menuProdutos);
+
+        let escolha = readline.question('Digite o número do produto desejado: ');
+
+        if (escolha === 'cancelar') {
+          console.log('Pedido cancelado.');
+          break;
+        } else if (parseInt(escolha) >= 1 && parseInt(escolha) <= this.menuCardapio.produtos.length) {
+          const produtoEscolhido = this.menuCardapio.obterProduto(parseInt(escolha));
+          console.log(`Produto escolhido: ${produtoEscolhido.nome}`);
+          this.carrinho.push(produtoEscolhido);
+          console.log('Produto adicionado ao carrinho.');
+          // Adicione a lógica para o próximo estágio ou ação
+          this.numero_estagio = 5;
         } else {
           console.log('Opção inválida. Por favor, tente novamente.');
         }
-      }
-
-      // Adicione os outros estágios e suas respectivas lógicas aqui
-
-      // Exemplo de saída do loop após um estágio
-      if (this.numero_estagio === 5) {
+      } else if (this.numero_estagio === 5) {
         console.log('\nEstágio 5: Pedido adicionado ao carrinho');
+        console.log('Carrinho:');
+        this.carrinho.forEach((produto, index) => {
+          console.log(`${index + 1}. ${produto.nome} - R$ ${produto.preco}`);
+        });
         break;
       }
     }
 
-    console.log('Chat finalizado. Obrigado por utilizar o Robô Groundon!');
+    console.log('Chat finalizado. Obrigado por utilizar o Bot Groundon!');
   }
 }
 
-
-async function main(){
+function main() {
   const usuario = new Usuario();
   usuario.iniciarChat();
-  
 }
 
+main();
---
+++ Chatbot - Delivary e Entregas/Chatbot Rayquaza x Groundon x Kyogre/Rayquaza/src/Teoria do Caos/CHATBOT - IA - TERMINAL/Models/Pedido/Pedido.js
@@ -0,0 +1,33 @@
+const produtos_cardapio = require("../../../../repository/cardapio_1.json"); // Importe o arquivo JSON do cardápio
+
+class Pedido {
+  constructor() {
+    this.produtos = [];
+    this.produtos_cardapio = produtos_cardapio;
+
+
+    this.pedido_cliente = {
+      nome: this.getNome(),
+      telefone: this.getPhoneNumber(),
+      //pedido: this.carrinho.verCarrinho(),
+      //carrinho: this.verCarrinhoCliente(),
+      pagamento: this.forma_pagamento,
+      endereco: this.endereco_cliente
+    };
+  }
+
+  getNome() {
+    return this.nome;
+  }
+
+  getPhoneNumber() {
+    return this.telefone;
+  }
+
+  adicionarProduto(produto) {
+    this.produtos.push(produto);
+  }
+}
+
+
+module.exports = Pedido
