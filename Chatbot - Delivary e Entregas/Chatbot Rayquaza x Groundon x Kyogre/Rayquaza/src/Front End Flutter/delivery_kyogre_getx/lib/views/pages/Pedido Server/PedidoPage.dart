052041e5ab602048291fdad57ec04d1a2112e5f9
PedroVic12 <pedrovictor.rveras12@gmail.com>
2023-07-25 09:07:38 -0300

    .

--- Chatbot - Delivary e Entregas/Chatbot Rayquaza x Groundon x Kyogre/Rayquaza/src/Front End Flutter/delivery_kyogre_getx/lib/views/pages/Pedido Server/PedidoPage.dart
+++ Chatbot - Delivary e Entregas/Chatbot Rayquaza x Groundon x Kyogre/Rayquaza/src/Front End Flutter/delivery_kyogre_getx/lib/views/pages/Pedido Server/PedidoPage.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: non_constant_identifier_names
 
-import 'package:delivery_kyogre_getx/views/Dashboard/Pedido/PedidoControler.dart';
+import 'package:delivery_kyogre_getx/views/pages/Dashboard/Pedido/PedidoControler.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
