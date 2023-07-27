bb21916f66422719292da5d9059b588609d43a8e
PedroVic12 <pedrovictor.rveras12@gmail.com>
2023-07-27 13:13:53 -0300

    .

--- Chatbot - Delivary e Entregas/Chatbot Rayquaza x Groundon x Kyogre/Rayquaza/src/Server Python/json_encode.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def process_json_file(json_file):
-    with open(json_file, "r") as file:
-        json_data = json.load(file)
-        print('\nRequisição =', json_data)
-
-process_json_file('Server Python/repository/pedido_10-06-23.json')
